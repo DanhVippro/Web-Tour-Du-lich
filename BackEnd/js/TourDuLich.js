6d07e1bea4c054579b3231c42184eaf1eaf6e121
Nhut Tan <tanphan131006@gmail.com>
2026-05-09 23:58:51 +0700

    hiii

--- BackEnd/js/TourDuLich.js
+++ BackEnd/js/TourDuLich.js
@@ -12,4 +12,38 @@ function showContent(type, el) {
 
    
     el.classList.add("active");
+
+    if (type === "trongnuoc") {
+
+       
+        document.getElementById("bac").style.display = "block";
+        document.getElementById("trung").style.display = "none";
+        document.getElementById("nam").style.display = "none";
+
+        
+        let subTabs = document.querySelectorAll(".subtab");
+
+        subTabs.forEach(t => t.classList.remove("active"));
+
+        subTabs[0].classList.add("active");
+    }
+
+
+
+}
+function showRegion(region, el) {
+
+  
+    document.getElementById("bac").style.display = "none";
+    document.getElementById("trung").style.display = "none";
+    document.getElementById("nam").style.display = "none";
+
+    
+    document.getElementById(region).style.display = "block";
+
+    let subTabs = document.querySelectorAll(".subtab");
+
+    subTabs.forEach(t => t.classList.remove("active"));
+
+    el.classList.add("active");
 }
